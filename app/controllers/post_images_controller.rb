7082778050f9117677d64b4affbbd0e772a648db
KoheiWatanabe <161041321+WatanabeKMF@users.noreply.github.com>
2024-04-01 14:16:59 +0000

    [Finish]4-20

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,15 +16,19 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
   
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end  
   # 投稿データのストロングパラメータ
   private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
-  
-  
 end
